55afc01dcfb41ad3c07d7cf27255af1f0bf59b69
winston salem <git8cmd@gmail.com>
2021-07-24 11:24:26 -0300

    relacionamento muitos para muitos

--- src/main/java/br/com/gulafood/model/Restaurante.java
+++ src/main/java/br/com/gulafood/model/Restaurante.java
@@ -1,5 +1,36 @@
 package br.com.gulafood.model;
 
-public class Restaurante {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data; 
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Restaurante implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private BigDecimal taxaFrete;
+	
+	/* relacionamento muitos para muitos e decalarando o nome das chaves estrangeiras na tabela intermediaria */
+	@ManyToMany 
+	@JoinTable(name = "restaurante_forma_pagamento",joinColumns = @JoinColumn(name ="restaurante_id"),
+	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
