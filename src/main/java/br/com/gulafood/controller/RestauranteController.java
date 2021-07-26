7dcf4a056cf99b5bdb7764fa4a1368ca631ff855
eduardo-sc <eduardo.santana95@hotmail.com>
2021-07-26 19:26:57 -0300

    nova atualizacao

--- src/main/java/br/com/gulafood/controller/RestauranteController.java
+++ src/main/java/br/com/gulafood/controller/RestauranteController.java
@@ -1,5 +1,15 @@
 package br.com.gulafood.controller;
 
-public class RestauranteController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.gulafood.Repository.RestauranteRepository;
+
+@Controller
+@RequestMapping
+public class RestauranteController {
+	@Autowired
+	private  RestauranteRepository restauranteRepository;
+	
 }
