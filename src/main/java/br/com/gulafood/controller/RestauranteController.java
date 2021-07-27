518b9850a9b36fd6ffccbe486541426bacecd5cc
eduardo-sc <eduardo.santana95@hotmail.com>
2021-07-27 20:17:43 -0300

    teste

--- src/main/java/br/com/gulafood/controller/RestauranteController.java
+++ src/main/java/br/com/gulafood/controller/RestauranteController.java
@@ -1,15 +1,48 @@
 package br.com.gulafood.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gulafood.Repository.RestauranteRepository;
+import br.com.gulafood.model.Restaurante;
 
-@Controller
-@RequestMapping
+
+/**
+ * 
+ * @author Eduardo Santana da Cruz
+ *
+ */
+
+@RestController
+@RequestMapping("/restaurante")
 public class RestauranteController {
 	@Autowired
-	private  RestauranteRepository restauranteRepository;
+	private RestauranteRepository restauranteRepository;
+
 	
+	@GetMapping
+	List<Restaurante> listar() {
+		return restauranteRepository.findAll();
+	}
+	
+	@GetMapping("/{buscaId}")
+	public ResponseEntity<?>buscarPorId(@PathVariable Long buscaId){
+		Optional<Restaurante> restaurante = restauranteRepository.findById(buscaId);
+		
+		
+		if (restaurante.isPresent()) {
+			return ResponseEntity.ok(restaurante.get());
+			
+		}
+		return ResponseEntity.notFound().build();
+	
+	}
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,11 @@
 insert into cozinha (nome) values ('japonesa');
 insert into cozinha (nome) values ('brasileira');
+insert into cozinha (nome) values ('lanches');
+
+
+insert into forma_pagamento(forma)values('Dinheiro');
+insert into forma_pagamento(forma)values('Cartao de Credito');
+insert into forma_pagamento(forma)values('Cartao de Debito');
+
+
+insert into Restaurante(nome,taxa_frete,cozinha_id)values('Macdonalds',5.00,3);
