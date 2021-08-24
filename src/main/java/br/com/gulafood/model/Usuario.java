3305f778b9cd95d25809f9c1bd66c36cd6e7f0ea
winston salem <git8cmd@gmail.com>
2021-08-24 18:23:53 -0300

    endereço

--- src/main/java/br/com/gulafood/model/Usuario.java
+++ src/main/java/br/com/gulafood/model/Usuario.java
@@ -1,11 +1,12 @@
 package br.com.gulafood.model;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,54 +21,53 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-public class Usuario implements Serializable{
+
+public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false, length = 40)
 	private String nome;
-	
+
 	@Column(nullable = false)
 	private String email;
-	
+
 	@Column(nullable = false, length = 6)
 	private String senha;
-	
-	
 
 	@Column(length = 12)
 	private String telefone;
 	
-	@JsonFormat(pattern = "dd/MM/yyyy")
-	private LocalDate dataCadastro;
+	@JsonIgnore
+	@Embedded // indicando que esta propriedade e de um tipo incorporando a classe restaurante 
+	private Endereco endereco;//Eduardo 
 	
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDateTime dataCadastro;
+
+	
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name = "usuario_grupo" , joinColumns = @JoinColumn(name = "usuario_id"),
-				inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
 	private List<Grupo> grupos = new ArrayList<>();
 	
 	
-	
+
 	@PrePersist // coloca a data de cadatro do sistema
 	public void prePersist() {
-		
-		setDataCadastro(LocalDate.now());
-		
+
+		setDataCadastro(LocalDateTime.now());
+
 	}
-	
-	
-	
+
 }
