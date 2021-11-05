28b53d1c10da954b6bbe2c2ced68ae2762edd8f9
winston salem <winstonwill94@gmail.com>
2021-11-04 21:58:56 -0300

    atualiaz

--- src/main/java/br/com/gulafood/model/Produto.java
+++ src/main/java/br/com/gulafood/model/Produto.java
@@ -4,12 +4,11 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@CrossOrigin("*")
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
