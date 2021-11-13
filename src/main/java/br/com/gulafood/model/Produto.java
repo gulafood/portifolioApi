7cfafefb8a93dc1616da44030ef09b9d019e34d2
winston salem <winstonwill94@gmail.com>
2021-11-13 13:20:01 -0300

    upp

--- src/main/java/br/com/gulafood/model/Produto.java
+++ src/main/java/br/com/gulafood/model/Produto.java
@@ -21,10 +21,9 @@ import lombok.NoArgsConstructor;
  */
 
 @Entity
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
-@CrossOrigin("*")
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -40,5 +39,78 @@ public class Produto implements Serializable{
 	
 	private byte[] foto;
 
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+
+
+	public byte[] getFoto() {
+		return foto;
+	}
+
+
+
+	public void setFoto(byte[] foto) {
+		this.foto = foto;
+	}
+
+	
 	
 }
